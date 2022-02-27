07461361577a5e753cae118e316adcfff0c38b89
sibasmarak <sibasmarak.p@gmail.com>
2022-02-28 02:06:10 +0530

    added README and yml file

---
+++ README.md
@@ -0,0 +1,9 @@
+## Commands to run  
+**Note**: The commands are incomplete.  
+
+```
+  conda env create -f retrieval.yml
+  conda activate retrieval
+```
+- `python data/preprocess_data.py` (to create the required data loading format)  
+- `python run.py` (to train/evaluate)
--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -1,20 +1,19 @@
-import os, json
 from tqdm import tqdm
+import os, json, argparse, csv
 
 def preprocess(data_path, dataset_name = "coco"):
-
-    data = json.load(open(data_path, "rb"))
     preprocessed_dict = {}
 
     if dataset_name == "coco":
 
         for image_dict in tqdm(data["images"], position = 0, leave = True):
+            data = json.load(open(data_path, "r"))
             Id = image_dict["id"]
             image_path = image_dict["file_name"]
             if "train" in image_path:
-                dir_path = "./dataset/coco/train2014"
+                dir_path = "./datasets/coco/train2014"
             elif "val" in image_path:
-                dir_path = "./dataset/coco/val2014"
+                dir_path = "./datasets/coco/val2014"
 
             preprocessed_dict[Id] = {"image_path": os.path.join(dir_path, image_path), "captions": []}
 
@@ -24,11 +23,12 @@ def preprocess(data_path, dataset_name = "coco"):
             preprocessed_dict[image_id]["captions"].append(caption)
 
     elif dataset_name == "flickr30k":
+        data = json.load(open(data_path, "r"))
 
         for image_dict in tqdm(data["images"], position = 0, leave = True):
             Id = image_dict["imgid"]
             image_path = image_dict["file_name"]
-            dir_path = "./dataset/flickr30/flickr30k-images"
+            dir_path = "./datasets/flickr30/flickr30k-images"
             preprocessed_dict[Id] = {"image_path": os.path.join(dir_path, image_path), "captions": []}
             sentences_list = image_dict["sentences"]
             for token_sent_dict in sentences:
@@ -37,9 +37,23 @@ def preprocess(data_path, dataset_name = "coco"):
                 preprocessed_dict[image_id]["captions"].append(caption)
 
     elif dataset_name == "cc":
-        pass
+        with open(data_path) as f:
+            tsv_file = csv.reader(f, delimiter="\t")
+            for line in tsv_file:
+                print(line)
+                exit(0)
+
+        
 
     return preprocessed_dict
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default = 'coco', type = str, choices=['cc', 'coco', 'flick30k'])
+    parser.add_argument('--data_path', type = str)
+
+    args = parser.parse_args()
+
+    preprocessed_dict = preprocess(args.data_path, args.dataset)
+    json.dump(preprocessed_dict, open(os.path.join('./datasets', f'{args.dataset}', 'image_captions.json')))
 
