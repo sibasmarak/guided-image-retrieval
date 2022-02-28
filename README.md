392ed76ccc5addda0317eb9019260bd3b5b18a2e
sibasmarak <sibasmarak.p@gmail.com>
2022-02-28 20:18:38 +0530

    cc dataset creation implemented

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
   conda activate retrieval
 ```
 - `python data/preprocess_data.py` (to create the required data loading format)  
+  - `python data/preprocess_data.py --dataset cc --data_path datasets/cc/Validation_GCC-1.1.0-Validation.tsv --split val`
+  - `python data/preprocess_data.py --dataset cc --data_path datasets/cc/Train_GCC-training.tsv --split train`
 - `python run.py` (to train/evaluate)
--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -1,4 +1,8 @@
+import requests
+import urllib.request
+from PIL import Image
 from tqdm import tqdm
+from io import BytesIO
 import os, json, argparse, csv
 
 def preprocess(data_path, dataset_name = "coco"):
@@ -37,13 +41,44 @@ def preprocess(data_path, dataset_name = "coco"):
                 preprocessed_dict[image_id]["captions"].append(caption)
 
     elif dataset_name == "cc":
-        with open(data_path) as f:
-            tsv_file = csv.reader(f, delimiter="\t")
-            for line in tsv_file:
-                print(line)
-                exit(0)
 
-        
+        if 'Train' in data_path:
+            dir_path = "./datasets/cc/train"
+        elif 'Validation' in data_path:
+            dir_path = "./datasets/cc/val"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        url_to_Id = dict()
+
+        with open(data_path) as f:
+            num_examples = len(f.readlines())
+            f.seek(0)
+            
+            tsv_file = csv.reader(f, delimiter="\t")
+            failed = 0
+
+            for Id, line in enumerate(tsv_file):
+                caption = line[0]
+                url = line[1]
+
+                if url not in list(url_to_Id.keys()):
+                    url_to_Id[url] = Id
+                    image_path = os.path.join(dir_path, f'{Id}.jpg')
+                    preprocessed_dict[Id] = {"image_path": image_path, "captions": [caption]}
+                else:
+                    Id = url_to_Id[url]
+                    preprocessed_dict[Id]["captions"].append(caption)
+                    continue
+
+                try:
+                    im = Image.open(requests.get(url, stream=True).raw)
+                    im.save(image_path)
+                except:
+                    print(f'Failed! Id: [{Id + 1}|{num_examples}] URL: {url}')
+                    failed += 1
+
+            print(f'Total failed: [{failed}|{num_examples}]')
 
     return preprocessed_dict
 
@@ -51,9 +86,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default = 'coco', type = str, choices=['cc', 'coco', 'flick30k'])
     parser.add_argument('--data_path', type = str)
+    parser.add_argument('--split', default='train', type = str, choices=['train', 'val', 'test'])
 
     args = parser.parse_args()
 
     preprocessed_dict = preprocess(args.data_path, args.dataset)
-    json.dump(preprocessed_dict, open(os.path.join('./datasets', f'{args.dataset}', 'image_captions.json')))
+    save_path = open(f'./datasets/{args.dataset}/{args.split}_image_captions.json', 'w')
+    json.dump(preprocessed_dict, save_path)
 
