aae35a654e5fc46449f59c8c9f308f7c810d6de2
shubhraneel <shubhraneelpal@gmail.com>
2022-04-06 15:03:42 +0530

    demo script

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,6 +1,8 @@
 # Import the required libraries
 import os
 import json
+from sre_parse import Tokenizer
+from data.preprocess_data import preprocess
 import torch
 import warnings
 import regex as re
@@ -10,6 +12,51 @@ from torchvision import transforms
 from transformers import AutoTokenizer
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 from utils.env import model_modelpath_mapping, model_with_no_token_types
+import numpy as np
+
+def preprocess_text(caption, language_model_name = "bert", max_length_caption=64, local_files_only=True):
+		language_model_path = model_modelpath_mapping[language_model_name]
+		tokenizer = AutoTokenizer.from_pretrained(language_model_path, local_files_only = local_files_only, TOKENIZERS_PARALLELISM = True)
+		caption = preprocess_caption(caption)
+		encoded_caption = tokenizer(caption, max_length=max_length_caption, padding='max_length', truncation=True)
+		caption_input_ids = encoded_caption['input_ids']
+		caption_attention_masks = encoded_caption['attention_mask']
+		caption_input_ids = torch.tensor([caption_input_ids]).squeeze()
+		caption_attention_masks = torch.tensor([caption_attention_masks]).squeeze()
+		caption_token_type_ids = None
+		if language_model_name not in model_with_no_token_types:
+			caption_token_type_ids = encoded_caption['token_type_ids']
+			caption_token_type_ids = torch.tensor([caption_token_type_ids]).squeeze()
+
+		caption_dict = {
+			'caption_input_ids': caption_input_ids,
+			'caption_attention_masks': caption_attention_masks,
+			'caption_token_type_ids': caption_token_type_ids,
+		}
+		return caption_dict
+
+
+def preprocess_caption(caption):
+
+		caption = re.sub('[^a-zA-Z]', ' ', caption)
+			
+		url = re.compile(r'https?://\S+|www\.\S+')
+		caption = url.sub(r'', caption)
+		
+		html = re.compile(r'<.*?>')
+		caption = html.sub(r'', caption)
+
+		emoji_pattern = re.compile("["
+								u"\U0001F600-\U0001F64F"  # emoticons
+								u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+								u"\U0001F680-\U0001F6FF"  # transport & map symbols
+								u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+								u"\U00002702-\U000027B0"
+								u"\U000024C2-\U0001F251"
+								"]+", flags=re.UNICODE)
+		caption = emoji_pattern.sub(r'', caption)
+
+		return caption
 
 # Image Caption Dataset for coco, flickr30k, conceptual captions
 class ImageCaptionDataset(Dataset):
@@ -108,7 +155,7 @@ class ImageCaptionDataset(Dataset):
 			caption_token_type_ids = torch.tensor(caption_token_type_ids).squeeze()
 
 
-		return image_id, image, caption_input_ids, caption_attention_masks, caption_token_type_ids
+		return image_id, image, caption_input_ids, caption_attention_masks, caption_token_type_ids, image_path
 
 	def collater(self, items):
 		
@@ -118,36 +165,20 @@ class ImageCaptionDataset(Dataset):
 				'images': torch.stack([x[1] for x in items], dim=0),
 				'caption_input_ids': torch.stack([x[2] for x in items], dim=0),
 				'caption_attention_masks': torch.stack([x[3] for x in items], dim=0),
-				'caption_token_type_ids': torch.stack([x[4] for x in items], dim=0)
+				'caption_token_type_ids': torch.stack([x[4] for x in items], dim=0),
+				'image_path': np.array([x[5] for x in items]),
 			}
 		else:
 			batch = {
 				'image_ids': torch.stack([x[0] for x in items], dim=0),
 				'images': torch.stack([x[1] for x in items], dim=0),
 				'caption_input_ids': torch.stack([x[2] for x in items], dim=0),
-				'caption_attention_masks': torch.stack([x[3] for x in items], dim=0)
+				'caption_attention_masks': torch.stack([x[3] for x in items], dim=0),
+				'image_path': np.array([x[5] for x in items]),
 			}
 
 		return batch
 	
 	def preprocess_caption(self, caption):
 
-		caption = re.sub('[^a-zA-Z]', ' ', caption)
-		
-		url = re.compile(r'https?://\S+|www\.\S+')
-		caption = url.sub(r'', caption)
-		
-		html = re.compile(r'<.*?>')
-		caption = html.sub(r'', caption)
-
-		emoji_pattern = re.compile("["
-						   u"\U0001F600-\U0001F64F"  # emoticons
-						   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-						   u"\U0001F680-\U0001F6FF"  # transport & map symbols
-						   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-						   u"\U00002702-\U000027B0"
-						   u"\U000024C2-\U0001F251"
-						   "]+", flags=re.UNICODE)
-		caption = emoji_pattern.sub(r'', caption)
-
-		return caption
+		return preprocess_caption(caption)
--- utils/demo.py
+++ utils/demo.py
@@ -1,40 +1,61 @@
+# Import the required libraries
+import torch
 from tqdm import tqdm
 import torch.nn.functional as F
-import torch
+import numpy as np
+import shutil
 
-def test_retrieval(model, dataloader, device, k_s = [1, 5]): # dataloader should have shuffle = False
-    # inner_dataloader = deepcopy(dataloader)
+def demo_retrieval(model, sentence, dataloader, device, k = 5, train_siam = True): # dataloader should have shuffle = False
+
     model.to(device)
     model.eval()
-    total_recalls = [0]*len(k_s)
+
     len_recall = 0
-    for batch in tqdm(dataloader):
-        with torch.no_grad():
-            text_features = model.language_model(
-                batch['caption_input_ids'].to(device), 
-                attention_masks = batch['caption_attention_masks'].to(device), 
-                token_type_ids = batch['caption_token_type_ids'].to(device)
+    
+    with torch.no_grad():
+        
+        if train_siam:
+            text_features = model.dual_encoder.language_model(
+                sentence['caption_input_ids'].to(device), 
+                attention_masks = sentence['caption_attention_masks'].to(device), 
+                token_type_ids = sentence['caption_token_type_ids'].to(device)
             )
-        text_features = F.normalize(text_features.cpu(), p=2, dim=1)
-        similarities = []
-        all_ids = []
-        for batch_in in dataloader:
-            with torch.no_grad():
+        else:
+            text_features = model.language_model(
+                sentence['caption_input_ids'].to(device), 
+                attention_masks = sentence['caption_attention_masks'].to(device), 
+                token_type_ids = sentence['caption_token_type_ids'].to(device)
+            )
+    
+    text_features = F.normalize(text_features.cpu(), p=2, dim=1)
+
+    similarities = []
+    all_ids = []
+    all_images = []
+    
+    for batch_in in dataloader:
+    
+        with torch.no_grad():
+            if train_siam:
+                image_features = model.dual_encoder.vision_model(batch_in['images'].to(device))
+            else:
                 image_features = model.vision_model(batch_in['images'].to(device))
-            image_features = F.normalize(image_features.cpu(), p=2, dim=1)
-            all_ids.append(batch_in['image_ids'])
-            sim_scores = torch.matmul(image_features, text_features.T)
-            similarities.append(sim_scores)
-        similarities = torch.cat(similarities, dim=0).T
-        ranked_indexes = torch.argsort(similarities, dim=1)
-        all_ids = torch.cat(all_ids, dim=0)
-        ranked_ids = all_ids[ranked_indexes]
-        curr_img_ids = batch['image_ids']
-        curr_img_ids = curr_img_ids.unsqueeze(dim=0).T.repeat(1, ranked_ids.size(1))
-        retrieval = torch.where(ranked_ids == curr_img_ids, 1, 0)
-        for k_i, k in enumerate(k_s):
-            total_recalls[k_i] += torch.sum(retrieval[:, :k])
-        len_recall += retrieval.size(0)
-    recalls = [float(total_recall)/len_recall for total_recall in total_recalls]
+
+        image_features = F.normalize(image_features.cpu(), p=2, dim=1)
+        all_ids.append(batch_in['image_ids'])
+        all_images.append(batch_in['image_path'])
+    
+        sim_scores = torch.matmul(image_features, text_features.T)
+        similarities.append(sim_scores)
+    
+    similarities = torch.cat(similarities, dim=0).T
+    ranked_indexes = torch.argsort(similarities, dim=1)
+    all_ids = torch.cat(all_ids, dim=0)
+    all_images = np.concatenate(all_images, dim=0)
+    ranked_images = all_images[ranked_indexes.cpu().numpy()]
+    image_paths = ranked_images[0, :k]
+    for i, image_path in enumerate(list(image_paths)):
+      shutil.copy(image_path, f"results/{i}.jpg")
+    
     model.train()
-    return recalls
+    
